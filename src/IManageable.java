a3a7ee957bdbe90fa398b90ac61589c91847b4c9
MuthuiDaniel <wambua.daniel@strathmore.edu>
2024-04-21 19:37:27 +0300

    updated the updateperson with properties and invaliddataexcpetion

--- src/IManageable.java
+++ src/IManageable.java
@@ -1,11 +1,11 @@
 interface IManageable {
-    void addPerson(Person person);
+    Person addPerson(Person person);
     void removePerson(String ID);
-    void updatePerson(Person person);
-    void addCourse(Course course);
+    void updatePerson(String ID, String newName, int newAge) throws InvalidDataException;
+    Course addCourse(Course course);
     void removeCourse(String courseCode);
     void updateCourse(Course course);
-    void addDepartment(Department department);
+    Department addDepartment(Department department);
     void removeDepartment(String departmentName);
     void updateDepartment(Department department);
 }
