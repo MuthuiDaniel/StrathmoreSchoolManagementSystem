64bbd049fa26bfa4d2de06de98b93021c9e7464b
MuthuiDaniel <wambua.daniel@strathmore.edu>
2024-04-21 19:40:01 +0300

    able to update department,course,person

--- src/Management.java
+++ src/Management.java
@@ -2,73 +2,80 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Management implements IManageable {
-    private List<Person> persons;
+    private final List<Person> persons;
+    private final List<Course> courses;
+    private final List<Department> departments;
 
     public Management() {
         this.persons = new ArrayList<>();
+        this.courses = new ArrayList<>();
+        this.departments = new ArrayList<>();
     }
 
     @Override
-    public void addPerson(Person person) {
+    public Person addPerson(Person person) {
         persons.add(person);
+        return person;
     }
 
     @Override
     public void removePerson(String ID) {
-        persons.removeIf(person -> person.ID.equals(ID));
+        persons.removeIf(person -> person.getID().equals(ID));
     }
 
     @Override
-    public void updatePerson(Person person) {
-        public void updatePerson(String ID, String newName, int newAge) {
-            for (Person person : persons) {
-                if (person.getID().equals(ID)) {
-                    person.setName(newName);
-                    person.setAge(newAge);
-                    break;
-                }
+    public void updatePerson(String ID, String newName, int newAge) throws InvalidDataException {
+        for (Person person : persons) {
+            if (person.getID().equals(ID)) {
+                person.setName(newName);
+                person.setAge(newAge);
+                return;
             }
         }
-
+        throw new InvalidDataException("Person with ID " + ID + " not found.");
     }
 
     @Override
-    public void addCourse(Course course) {
-
+    public Course addCourse(Course course) {
+        courses.add(course);
+        return course;
     }
 
     @Override
     public void removeCourse(String courseCode) {
-
+        courses.removeIf(course -> course.getCourseCode().equals(courseCode));
     }
 
     @Override
-    public void updateCourse(Course course) {
-
+    public void updateCourse(Course updatedCourse) {
+        for (int i = 0; i < courses.size(); i++) {
+            Course course = courses.get(i);
+            if (course.getCourseCode().equals(updatedCourse.getCourseCode())) {
+                courses.set(i, updatedCourse);
+                return;
+            }
+        }
     }
 
     @Override
-    public void addDepartment(Department department) {
-
+    public Department addDepartment(Department department) {
+        departments.add(department);
+        return department;
     }
 
     @Override
     public void removeDepartment(String departmentName) {
-
+        departments.removeIf(department -> department.getDepartmentName().equals(departmentName));
     }
 
     @Override
-    public void updateDepartment(Department department) {
-
-    }
-
-    //methods for managing courses, faculty, and departments
-    public void addCourseToDepartment(Course course, Department department) {
-        department.addCourse(course);
-    }
-
-    public void addFacultyToDepartment(Faculty faculty, Department department) {
-        department.addFacultyMember(faculty);
+    public void updateDepartment(Department updatedDepartment) {
+        for (int i = 0; i < departments.size(); i++) {
+            Department department = departments.get(i);
+            if (department.getDepartmentName().equals(updatedDepartment.getDepartmentName())) {
+                departments.set(i, updatedDepartment);
+                return;
+            }
+        }
     }
 }
-
