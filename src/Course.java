b41831fa1a85489012fa4aa5b1fea759e480aa88
MuthuiDaniel <wambua.daniel@strathmore.edu>
2024-04-21 16:58:46 +0300

    privatised coursename, coursecode, assignedfaculty, enrolledstudents

--- src/Course.java
+++ src/Course.java
@@ -1,4 +1,46 @@
 public class Course {
+    private String courseName;
+    private String courseCode;
+    private Faculty assignedFaculty;
+    private List<Student> enrolledStudents;
 
+    public Course(String courseName, String courseCode, Faculty assignedFaculty) {
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+        this.assignedFaculty = assignedFaculty;
+        this.enrolledStudents = new ArrayList<>();
+    }
 
+    // Getters and setters
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public Faculty getAssignedFaculty() {
+        return assignedFaculty;
+    }
+
+    public void setAssignedFaculty(Faculty assignedFaculty) {
+        this.assignedFaculty = assignedFaculty;
+    }
+
+    public List<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void enrollStudent(Student student) {
+        enrolledStudents.add(student);
+    }
 }
