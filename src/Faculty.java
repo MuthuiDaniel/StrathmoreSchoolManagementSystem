e7ea7b7c83bdec0f370bfd1b2155ad5851f74716
MuthuiDaniel <wambua.daniel@strathmore.edu>
2024-04-21 19:52:31 +0300

    displayinfo provides implementation of specific details

--- src/Faculty.java
+++ src/Faculty.java
@@ -1,22 +1,9 @@
-import java.util.List;
-
 public class Faculty extends Person {
     private String department;
-    private List<String> coursesTaught;
 
     public Faculty(String name, int age, String ID, String department) {
         super(name, age, ID);
         this.department = department;
-        this.coursesTaught = coursesTaught;
-    }
-
-    @Override
-    public void displayInfo() {
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("ID: " + ID);
-        System.out.println("Department: " + department);
-        System.out.println("Courses Taught: " + coursesTaught);
     }
 
     public String getDepartment() {
@@ -27,11 +14,11 @@ public class Faculty extends Person {
         this.department = department;
     }
 
-    public List<String> getCoursesTaught() {
-        return coursesTaught;
-    }
-
-    public void setCoursesTaught(List<String> coursesTaught) {
-        this.coursesTaught = coursesTaught;
+    @Override
+    public void displayInfo() {
+        System.out.println("Name: " + getName());
+        System.out.println("Age: " + getAge());
+        System.out.println("ID: " + getID());
+        System.out.println("Department: " + department);
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,45 +1,19 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Student extends Person {
     private double GPA;
-    private List<Course> enrolledCourses;
+    private List<String> coursesEnrolled;
 
-    public Student(String name, int age, String ID, double GPA) {
+    public Student(String name, int age, String ID, double GPA, List<String> coursesEnrolled) {
         super(name, age, ID);
         this.GPA = GPA;
-        this.enrolledCourses = new ArrayList<>();
+        this.coursesEnrolled = coursesEnrolled;
     }
 
     @Override
-    public void displayInfo() {
-        System.out.println("Name: " + name);
+    void displayInfo() {
+        System.out.println("Student Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("ID: " + ID);
         System.out.println("GPA: " + GPA);
-        System.out.println("Enrolled Courses: ");
-        for (Course course : enrolledCourses) {
-            System.out.println(course.getCourseName() + " (" + course.getCourseCode() + ")");
-        }
-    }
-
-    public double getGPA() {
-        return GPA;
-    }
-
-    public void setGPA(double GPA) {
-        this.GPA = GPA;
-    }
-
-    public List<Course> getEnrolledCourses() {
-        return enrolledCourses;
-    }
-
-    public void setEnrolledCourses(List<Course> enrolledCourses) {
-        this.enrolledCourses = enrolledCourses;
-    }
-
-    public void enrollCourse(Course course) {
-        enrolledCourses.add(course);
+        System.out.println("Courses Enrolled: " + coursesEnrolled);
     }
 }
