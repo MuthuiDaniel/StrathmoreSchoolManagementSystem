9e21ed709f4c1fb27891285f27a020816518dcc7
MuthuiDaniel <wambua.daniel@strathmore.edu>
2024-04-21 17:02:17 +0300

    privatisied department and list of courses as well as faculty members

--- src/Department.java
+++ src/Department.java
@@ -1,3 +1,40 @@
 public class Department {
+        private String departmentName;
+        private List<Course> courses;
+        private List<Faculty> facultyMembers;
 
+        public Department(String departmentName) {
+            this.departmentName = departmentName;
+            this.courses = new ArrayList<>();
+            this.facultyMembers = new ArrayList<>();
+        }
+
+        // Getters and setters
+        public String getDepartmentName() {
+            return departmentName;
+        }
+
+        public void setDepartmentName(String departmentName) {
+            this.departmentName = departmentName;
+        }
+
+        public List<Course> getCourses() {
+            return courses;
+        }
+
+        public void addCourse(Course course) {
+            courses.add(course);
+        }
+
+        public List<Faculty> getFacultyMembers() {
+            return facultyMembers;
+        }
+
+        public void addFacultyMember(Faculty faculty) {
+            facultyMembers.add(faculty);
+        }
+
+        public boolean isFacultyEnrolled(Faculty faculty) {
+            return facultyMembers.contains(faculty);
+        }
 }
